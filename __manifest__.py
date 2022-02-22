260722385147d9339420c54cc53e25dc746616ca
Adrian Muriel <adrianmurielzamora@hotmail.com>
2022-02-22 09:04:40 +0100

    Inicio del Proyecto

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Cinema",
+
+    'summary': """ Módulo de almacenamiento de películas """,
+
+    'description': """
+        Este módulo tiene como finalidad el almacenamiento de
+	peliculas indicando su Titulo, Descripción y Año de Estreno
+    """,
+
+    'author': "Adrián Muriel Zamora",
+    'website': "https://www.google.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['baset'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application' : True,
+}
